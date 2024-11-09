d469af7421a64d61db85493a1ded0c7c67e79ebb
Krishna <krishna.shrestha@racingandsports.com>
2024-11-09 16:29:44 +1100

    wip: starting gridview and card design

--- lib/features/home/views/home.dart
+++ lib/features/home/views/home.dart
@@ -20,7 +20,7 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
